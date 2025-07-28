0142cc380cf8373d23389decb50f833da1a1540e
Avantika1327 <avantika.kawale@gmail.com>
2025-07-28 15:32:07 +0530

    fix log issue

---
+++ storage/framework/views/cfa541986078230780a6a42d661ac77a.php
@@ -0,0 +1,15 @@
+
+<?php echo $__env->make('layout.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- Navigation -->
+<section class="topbar-container z-30">
+    <?php echo $__env->make('layout.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->make('layout.sidebar' , \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</section>
+
+<!-- Main Content -->
+<main class="main-content has-sidebar">
+    <?php echo $__env->yieldContent('content'); ?>
+</main>
+
+<?php echo $__env->make('layout.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH C:\Users\Avantika\office_work\Swiss\resources\views/layout/main.blade.php ENDPATH**/ ?>
