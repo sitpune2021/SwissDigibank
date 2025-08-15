15ed23390a7351712598ecb2725e45d9fcd272a5
Anuradha <aj2740@gmail.com>
2025-08-15 14:14:30 +0530

    changes done and solved issues

--- resources/views/company/director/index.blade.php
+++ resources/views/company/director/index.blade.php
@@ -95,19 +95,19 @@
                 @forelse ($directors as $index => $director)
                 <tr>
                     <td class="px-6 py-4">{{ $director->designation ?? 'N/A' }}</td>
-                    {{-- <td class="px-6 py-4">{{ $director->member->member_info_first_name ?? 'N/A' }}</td> --}}
+                    {{-- <td class="px-6 py-4">{{ $director->member?->member_info_first_name ?? 'N/A' }}</td> --}}
                      <td class="py-3 px-6">
                             @if ($director->member)
                             <a href="{{ route('member.show', $director->member->id) }}"
                                 class="text-primary hover:underline">
-                                DEMO-{{ $director->member->member_info_first_name }}
+                                DEMO-{{ $director->member?->member_info_first_name ??''}}
                             </a>
                             @else
                             N/A
                             @endif
                         </td>
-                    <td class="px-6 py-4">{{ $director->director_name ?? 'N/A' }}</td>
-                    <td class="px-6 py-4">{{ $director->din_no }}</td>
+                    <td class="px-6 py-4">{{ $director?->director_name ?? 'N/A' }}</td>
+                    <td class="px-6 py-4">{{ $director?->din_no??'' }}</td>
                     <td class="px-6 py-4">{{ $director->appointment_date?->format('d/m/Y') ?? 'N/A' }}</td>
                     <td class="px-6 py-4">{{ $director->resignation_date?->format('d/m/Y') ?? 'N/A' }}</td>
                     <!-- <td class="px-6 py-4">{{ $director->authorized_signatory ? 'Yes' : 'No' }}</td> -->
