82e88dcb4da38ee9f3339eb2d2f6720fcb95db8b
shrutika1099 <shrutikatalokar1099@gmail.com>
2025-09-26 18:32:09 +0530

    tab button ui changes

--- resources/views/members/member/otherChargesList.blade.php
+++ resources/views/members/member/otherChargesList.blade.php
@@ -16,7 +16,7 @@
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
         <a href="{{ route('members.other-charges', ['id' => $member->id]) }}"
-            class="btn-success rounded-md px-2 py-1 text-white text-sm bg-green-500 hover:bg-green-600">
+            class="btn-primary rounded-10 px-2 py-2 mb-4">
             DEBIT OTHER CHARGES
         </a>
 
@@ -26,7 +26,7 @@
                 'chargeId' => $charge->id ?? '',
             ])
             : '#' }}"
-            class="btn-warning rounded-md px-2 py-1 text-white text-sm bg-yellow-500 hover:bg-yellow-600">
+            class="btn-warning rounded-10 px-2 py-2 mb-4">
             CLEAR DUES
         </a>
         {{-- Table --}}
