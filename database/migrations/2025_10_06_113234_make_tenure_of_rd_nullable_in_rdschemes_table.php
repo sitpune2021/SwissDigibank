125de8cb743aaee871c88a83cd74566fe96a40f0
shrutika1099 <shrutikatalokar1099@gmail.com>
2025-10-15 11:51:52 +0530

    migration issue solve

--- database/migrations/2025_10_06_113234_make_tenure_of_rd_nullable_in_rdschemes_table.php
+++ database/migrations/2025_10_06_113234_make_tenure_of_rd_nullable_in_rdschemes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rdschemes', function (Blueprint $table) {
-            $table->integer('tenure_of_rd')->nullable()->change();
+            $table->integer('tenure_of_rd')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('rdschemes', function (Blueprint $table) {
-            $table->integer('tenure_of_rd')->nullable(false)->change();
+            $table->integer('tenure_of_rd');
         });
     }
 };
