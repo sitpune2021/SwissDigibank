aab03573254a1a57171d4ab96c76331b1b946286
Ravi Garur <ravi.gaur@avsstech.com>
2025-11-24 17:17:19 +0530

    Mortgage Account Section

---
+++ app/Models/MortgageLoanOtherCharge.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MortgageLoanOtherCharge extends Model
+{
+     protected $fillable = [
+        'loan_id',
+        'transaction_type',
+        'charge_type',
+        'amount',
+        'charge_date',
+        'remarks',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(MortgageLoanApplication::class, 'loan_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
---
+++ database/migrations/2025_11_24_121740_create_mortgage_loan_transactions_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mortgage_loan_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('loan_id'); // FK to MortgageLoanApplication
+            $table->integer('emi_no')->nullable();
+            $table->date('transaction_date')->nullable();
+            $table->decimal('current_debt', 15, 2)->default(0);
+            $table->decimal('other_charges', 15, 2)->default(0);
+            $table->decimal('total_payable', 15, 2)->default(0);
+            $table->decimal('amount_collected', 15, 2)->default(0);
+            $table->text('remarks')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->string('status')->default('pending');
+            $table->date('paid_date')->nullable();
+            $table->string('fee_mode')->nullable();
+            $table->unsignedBigInteger('bank_id')->nullable();
+            $table->string('cheque_no')->nullable();
+            $table->date('cheque_date')->nullable();
+            $table->string('utr_no')->nullable();
+            $table->string('flag')->nullable();
+            $table->string('transfer_mode')->nullable();
+            $table->boolean('credited_to_company')->default(false);
+            $table->timestamps();
+
+            // Foreign key
+            $table->foreign('loan_id')->references('id')->on('mortgage_loan_applications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mortgage_loan_transactions');
+    }
+};
