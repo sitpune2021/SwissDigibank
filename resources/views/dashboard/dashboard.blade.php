4d1866850a64a610599fa727d9cf8c993c913bff
Shekhar <shekharmudmesitsolution@gmail.com>
2026-02-06 16:52:53 +0530

    dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -570,6 +570,56 @@ use Illuminate\Support\Facades\Storage;
         </a>
     </div>
 
+    <!-- Consumer durable loan LOAN  -->
+    <div class="col-span-12 p-4 sm:col-span-3 xxxl:col-span-3 box bg-n0 dark:bg-bg4 4xl:px-8 4xl:py-6">
+        <a href="{{ route('vehical.account.index') }}">
+            <div class="flex items-center justify-between pb-4 mb-4 lg:mb-6 lg:pb-6 bb-dashed">
+                <span class="font-medium">CONSUMER DURABLE LOAN</span><span aria-expanded="false"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="cursor-pointer tabler-icon tabler-icon-dots">
+                        <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                        <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                        <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                    </svg></span>
+            </div>
+            <div class="flex items-center gap-4 xl:gap-6">
+                <div
+                    class="w-14 xl:w-[72px] h-14 xl:h-[72px] flex items-center justify-center bg-primary/5 text-primary border border-n30 dark:border-n500 rounded-xl">
+                    <i class="text-3xl las xl:text-5xl la-car"></i>
+                </div>
+                <div>
+                    <h4 class="mb-2 h4 xxl:mb-4">0</h4>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <!-- Agricultural LOAN  -->
+    <div class="col-span-12 p-4 sm:col-span-3 xxxl:col-span-3 box bg-n0 dark:bg-bg4 4xl:px-8 4xl:py-6">
+        <a href="{{ route('vehical.account.index') }}">
+            <div class="flex items-center justify-between pb-4 mb-4 lg:mb-6 lg:pb-6 bb-dashed">
+                <span class="font-medium">AGRICULTURAL LOAN</span><span aria-expanded="false"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="cursor-pointer tabler-icon tabler-icon-dots">
+                        <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                        <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                        <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                    </svg></span>
+            </div>
+            <div class="flex items-center gap-4 xl:gap-6">
+                <div
+                    class="w-14 xl:w-[72px] h-14 xl:h-[72px] flex items-center justify-center bg-primary/5 text-primary border border-n30 dark:border-n500 rounded-xl">
+                    <i class="text-3xl las xl:text-5xl la-car"></i>
+                </div>
+                <div>
+                    <h4 class="mb-2 h4 xxl:mb-4">0</h4>
+                </div>
+            </div>
+        </a>
+    </div>
+
   <!-- MIS REPORT  -->  
   {{--<div class="col-span-12 p-4 sm:col-span-3 xxxl:col-span-3 box bg-n0 dark:bg-bg4 4xl:px-8 4xl:py-6">
         <a href="{{ route('mis_index') }}">
