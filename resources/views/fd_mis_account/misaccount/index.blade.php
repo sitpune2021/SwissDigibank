f31051d52a4fe2c358f66c1530340118d43c2eeb
allein395 <bhaveshmane395@gmail.com>
2026-03-10 17:25:18 +0530

    forcloase and remove account functionality complete in fd account and mis account

--- resources/views/fd_mis_account/misaccount/index.blade.php
+++ resources/views/fd_mis_account/misaccount/index.blade.php
@@ -129,7 +129,8 @@
                             @elseif ($mis->status == 2)
                             <span class="block w-28 rounded-[30px] border border-n30 bg-error/20 py-2 text-error text-xs text-error dark:border-n500 dark:bg-bg3 xxl:w-16 text-center">
                                 Rejected
-                            </span>@elseif ($mis->status == 3)
+                            </span>
+                            @elseif ($mis->status == 3)
                             <span class="block w-28 rounded-[30px] border border-n30 bg-error/20 py-2 text-error text-xs text-error dark:border-n500 dark:bg-bg3 xxl:w-16 text-center">
                                 Foreclosed
                             </span>
