f14d02927388f306100562651e81e5447d2d0bd3
Shekhar <shekharmudmesitsolution@gmail.com>
2026-05-20 17:25:49 +0530

    button ui

--- config/menu.php
+++ config/menu.php
@@ -19,7 +19,7 @@ return [
             ['title' => 'PROMOTORS', 'route' => 'promotor.index'],
             ['title' => 'PROMOTOR SHARE HOLDINGS', 'route' => 'shareholding.index'],
             ['title' => 'DIRECTOR', 'route' => 'director.index'],
-            ['title' => 'UNENCUMBERED DEPOSITS', 'route' => 'unencumbered-deposits.index'],
+            //['title' => 'UNENCUMBERED DEPOSITS', 'route' => 'unencumbered-deposits.index'],
             ['title' => 'BANK ACCOUNT', 'route' => 'bank-account.index'],
 
         ],
